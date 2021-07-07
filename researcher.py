4195e77fa809342c9df8c20c8c48ae6777b1a071
swier <swier.garst@gmail.com>
2021-07-07 11:42:50 +0200

    some bugfixes and extra features in the researcher file

--- researcher.py
+++ researcher.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 sys.path.insert(1, os.path.join(sys.path[0], '../..'))
-from helper_functions import get_datasets, heatmap
+from helper_functions import get_config, heatmap, get_save_str
 from vantage6.client import Client
 import time
 from vantage6.tools.util import info
@@ -13,9 +13,7 @@ from io import BytesIO
 
 
 start_time = time.time()
-dataset = "MNIST_2class_IID"
-### connect to server
-datasets = get_datasets(dataset)
+
 #datasets.remove("/home/swier/Documents/afstuderen/nnTest/v6_simpleNN_py/local/MNIST_2Class_IID/MNIST_2Class_IID_client9.csv")
 
 print("Attempt login to Vantage6 API")
@@ -28,80 +26,103 @@ client.setup_encryption(privkey)
 ids = [org['id'] for org in client.collaboration.get(1)['organizations']]
 
 
-num_global_rounds = 100
+num_global_rounds = 10
 num_clients = 10
+num_runs = 4
+seed_offset = 0
 
+save_file = True
+class_imbalance = False
+sample_imbalance = False
 
+dataset = "MNIST_2class_IID"
+datasets, parameters, X_test, y_test, c, ci = get_config(dataset, num_clients, class_imbalance, sample_imbalance)
 
+week = "../datafiles/w11/"
+prefix = "Trees_IID_"
 
 parameters = [np.zeros((1,784)), np.zeros((1))]
-accuracies = np.zeros((num_global_rounds))
-coefs = np.zeros((num_clients, 784))
-intercepts = np.zeros((num_clients, 1))
+local_accuracies = np.zeros((num_runs, num_global_rounds))
+global_accuracies = np.zeros((num_runs, num_global_rounds))
+
 #map = heatmap(num_clients, num_global_rounds )
 
 
+for run in num_runs:
+    seed = run + seed_offset
 
-# first round is slightly different now
-first_round = client.post_task(
-        input_= {
-        'method' : 'create_first_tree'
-    },
-    name = "trees, first round",
-    image = "sgarst/federated-learning:fedTrees",
-    organization_ids=[ids[0]],
-    collaboration_id = 1
-)
-## aggregate responses for the first round
-
-info("Waiting for results")
-res = client.get_results(task_id=first_round.get("id"))
-attempts=1
-#print(res)
-while(res[0]["result"] == None  and attempts < 20):
-    print("waiting...")
-    time.sleep(1)
-    res = client.get_results(task_id=first_round.get("id"))
-    attempts += 1
-
-results = np.array(np.load(BytesIO(res[0]["result"]),allow_pickle=True), dtype=object)
-print(results)
-accuracies[0] = results[0]
-
-#print(results[0][1])
-model = results[1]
-print(model)
-
-for round in range(1, num_global_rounds):
-    print("starting round ", round)
-    round_task = client.post_task(
-        input_= {
-            'method' : 'create_other_trees',
+    # first round is slightly different now
+    first_round = client.post_task(
+            input_= {
+            'method' : 'create_first_tree',
             'kwargs' : {
-                'tree_num' : round + 1,
-                'model' : model
-                }
+                'seed' : seed
+            }
         },
-        name = "trees, round " + str(round),
+        name = "trees, first round",
         image = "sgarst/federated-learning:fedTrees",
-        organization_ids=[ids[round%num_clients]],
+        organization_ids=[ids[0]],
         collaboration_id = 1
     )
-    res = client.get_results(task_id=round_task.get("id"))
+    ## aggregate responses for the first round
+
+    info("Waiting for results")
+    res = client.get_results(task_id=first_round.get("id"))
     attempts=1
-    ## aggregate responses
-    while(res[0]["result"] == None and attempts < 20):
+    #print(res)
+    while(res[0]["result"] == None  and attempts < 20):
         print("waiting...")
         time.sleep(1)
         res = client.get_results(task_id=first_round.get("id"))
         attempts += 1
 
-
     results = np.array(np.load(BytesIO(res[0]["result"]),allow_pickle=True), dtype=object)
+    print(results)
+    local_accuracies[run, 0] = results[0]
 
-    print("got the results")
-    accuracies[round] = results[0]
+    #print(results[0][1])
     model = results[1]
+
+    for round in range(1, num_global_rounds):
+        print("starting round ", round)
+        round_task = client.post_task(
+            input_= {
+                'method' : 'create_other_trees',
+                'kwargs' : {
+                    'tree_num' : round + 1,
+                    'model' : model
+                    }
+            },
+            name = "trees, round " + str(round),
+            image = "sgarst/federated-learning:fedTrees",
+            organization_ids=[ids[round%num_clients]],
+            collaboration_id = 1
+        )
+        res = client.get_results(task_id=round_task.get("id"))
+        attempts=1
+        ## aggregate responses
+        while(res[0]["result"] == None and attempts < 20):
+            print("waiting...")
+            time.sleep(1)
+            res = client.get_results(task_id=round_task.get("id"))
+            attempts += 1
+
+
+        results = np.array(np.load(BytesIO(res[0]["result"]),allow_pickle=True), dtype=object)
+
+        print("got the results")
+        local_accuracies[run, round] = results[0]
+        model = results[1]
+        global_accuracies[run, round] = model.score(X_test, y_test)
+        if save_file:
+        ### save arrays to files
+            with open (week + prefix + "local_seed" + str(seed) + ".npy", 'wb') as f:
+                np.save(f, local_accuracies)
+            
+            with open (week + prefix + "global_seed" + str(seed) + ".npy", 'wb') as f:
+                np.save(f, global_accuracies)
+
+
     #print(trees)
     #print(model)
     #map.save_round(round, coefs, avg_coef, is_dict=False)
